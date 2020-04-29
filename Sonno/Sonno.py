7e95aa6721e02e73b008a5de6a5ee00cbf521f79
FraLeone837 <10572353@polimi.it>
2020-04-29 20:09:39 +0200

    first commit

---
+++ Sonno/Sonno.py
@@ -0,0 +1,120 @@
+import csv
+#from decimal import Decimal, ROUND_DOWN, ROUND_UP
+
+#set parameters
+probability_parameter = 120
+credibility_parameter = 10
+wake_probability_load = 0.8
+inputFile = "31-01febbraio.csv"
+outputFile = "FilteredBedSensorData.csv"
+
+#file settings
+inputFieldnames = ('id', 'id_node', 'time_packet', 'timestamp', 'HR', 'RR', 'SV', 'HRV', 'SS', 'status', 'b2b', 'b2b1', 'b2b2')
+outputFieldname = ('id', 'id_node', 'time_packet', 'HR', 'HR_credibility', 'RR', 'RR_credibility', 'status', 'status_probability_out', 'status_probability_in', 'status_probability_mov')
+with open(inputFile, mode = 'r') as i_File:
+    with open(outputFile, mode = 'w', newline = '') as o_File:
+        inputReader = csv.DictReader(i_File, fieldnames = inputFieldnames)
+        outputWriter = csv.DictWriter(o_File, fieldnames = outputFieldname, delimiter=',', quotechar='"', quoting = csv.QUOTE_ALL)
+
+#global variables initialization
+        previous_stati = []
+        previous_HRs = []
+        for x in range(credibility_parameter):
+            previous_HRs.append(0)
+        previous_RRs = []
+        for x in range(credibility_parameter):
+            previous_RRs.append(0)
+        data_counter = 0
+        prev_HR = 0
+        prev_RR = 0
+        HRcredibility_sum = 0
+        RRcredibility_sum = 0
+        #outputWriter.writeheader()
+
+        for row in inputReader:
+            data_counter += 1
+#first 120 inputs
+            if data_counter <= probability_parameter:
+
+                o_id = row['id']
+                o_id_node = row['id_node']
+                o_time_packet = row['time_packet']
+                previous_stati.append(int(row['status']))
+                o_status_probalility_out = ''
+                o_status_probalility_in = ''
+                o_status_probalility_mov = ''
+                o_status = ''
+                o_HR = ''
+                o_HR_credibility = ''
+                o_RR = ''
+                o_RR_credibility = ''
+
+#other inputs
+            elif data_counter > probability_parameter:
+#costants
+                o_id = row['id']
+                o_id_node = row['id_node']
+                o_time_packet = row['time_packet']
+#status and probabilities
+                previous_stati.pop(0)
+                previous_stati.append(int(row['status']))
+                zero_sum = 0
+                one_sum = 0
+                two_sum = 0
+                for x in previous_stati:
+                    if x == 0:
+                        zero_sum += 1
+                    elif x == 1:
+                        one_sum += 1
+                    else:
+                        two_sum += 1
+                o_status_probalility_out = zero_sum / probability_parameter
+                o_status_probalility_in = one_sum / probability_parameter
+                o_status_probalility_mov = two_sum / probability_parameter
+                if o_status_probalility_out > (wake_probability_load * (o_status_probalility_in + o_status_probalility_mov)):
+                    o_status = 0
+                elif int(row['status']) == 0:
+                    o_status = 1
+                else:
+                    o_status = int(row['status'])
+#HR, RR and credibilities
+                previous_HRs.pop(0)
+                if o_status == 0:
+                    o_HR = 0
+                    previous_HRs.append(1)
+                elif int(row['HR']) == 0:
+                    o_HR = prev_HR
+                    previous_HRs.append(0)
+                else:
+                    o_HR = int(row['HR'])
+                    previous_HRs.append(1)
+                prev_HR = o_HR
+                if o_status == 0:
+                    o_HR_credibility = 1
+                else:
+                    HRcredibility_sum = 0
+                    for x in previous_HRs:
+                        HRcredibility_sum += x
+                    o_HR_credibility = HRcredibility_sum / credibility_parameter
+                previous_RRs.pop(0)
+                if o_status == 0:
+                    o_RR = 0
+                    previous_RRs.append(1)
+                elif int(row['RR']) == 0:
+                    o_RR = prev_RR
+                    previous_RRs.append(0)
+                else:
+                    o_RR = int(row['RR'])
+                    previous_RRs.append(1)
+                prev_RR = o_RR
+                if o_status == 0:
+                    o_RR_credibility = 1
+                else:
+                    RRcredibility_sum = 0
+                    for x in previous_RRs:
+                        RRcredibility_sum += x
+                    o_RR_credibility = RRcredibility_sum / credibility_parameter
+#write results
+            outputWriter.writerow({'id': o_id, 'id_node': o_id_node, 'time_packet' : o_time_packet, 'HR' : o_HR, 'HR_credibility' : o_HR_credibility, 'RR' : o_RR, 'RR_credibility' : o_RR_credibility, 'status' : o_status, 'status_probability_out' : o_status_probalility_out, 'status_probability_in' : o_status_probalility_in, 'status_probability_mov' : o_status_probalility_mov})
+
+
